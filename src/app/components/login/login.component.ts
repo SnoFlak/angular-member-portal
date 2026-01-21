2a28f3f4899b111878a5eecd98d0cf9056a4d2b0
Mathias Sletten <mathiasasletten@gmail.com>
2026-01-20 19:27:28 -0600

    simple pages

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'login',
+    standalone: true,
+    templateUrl: './login.component.html'
+})
+export class LandingComponent {
+    
+}
